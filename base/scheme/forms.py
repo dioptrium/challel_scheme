94963359579d6c7214b55f74a3e62593c7669259
dioptrium <dioptium@gmail.com>
2022-02-17 23:42:02 +0400

    version 1.2

--- base/scheme/forms.py
+++ base/scheme/forms.py
@@ -1,7 +1,8 @@
 from ast import Eq
+from cProfile import label
 from django import forms
 from .models import Channels, Equipment, Locations, Specifications
-from django.forms import formset_factory, inlineformset_factory
+from django.forms import ModelMultipleChoiceField, formset_factory, inlineformset_factory
 
 
 class LocationForm(forms.ModelForm):
@@ -19,15 +20,10 @@ class EquipmentForm(forms.ModelForm):
         widgets = {
            'equipment': forms.TextInput(attrs={'size':60}),
            'description': forms.TextInput(attrs={'size':60}),}
-    equipment_queryset = Equipment.objects.order_by('locations_connect')
-    equipment_connect= forms.ModelMultipleChoiceField(queryset=equipment_queryset, widget=forms.CheckboxSelectMultiple)
-
-
+    
 
 
 EquipmentInlineFormset = inlineformset_factory(Locations, Equipment, form=EquipmentForm, extra=1)
-
-
 
 
 '''Переопределение ModelMultipleChoiceField
@@ -157,18 +153,24 @@ class ChannelForm(forms.ModelForm):
             queryset=Equipment.objects.all(),
             widget=forms.CheckboxSelectMultiple(),
             required=False)'''
-        
+
+class CustomSelectMultiple(ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" %(obj.equipment, obj.locations_connect)
+        # return "%s" %(obj.name)
+    
 class ChannelForm(forms.ModelForm):
-    #field = forms.CharField(label='Your name', max_length=100)
+    
+    equipment_queryset = Equipment.objects.order_by ('locations_connect_id')
+    equipment_connect= CustomSelectMultiple(queryset=equipment_queryset, label="fff", widget=forms.CheckboxSelectMultiple)
+    #equipment_connect= forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[(c.locations_connect, c.equipment) for c in Equipment.objects.all()],)
+    locations_connect_check = Locations.objects.all()
     class Meta:
         model=Channels
         fields =['channel_name', 'object_a', 'object_b','traffic','description','equipment_connect',]
 
-    equipment_queryset = Equipment.objects.order_by('-locations_connect')
     
    
-    equipment_connect= forms.ModelMultipleChoiceField(queryset=equipment_queryset, widget=forms.CheckboxSelectMultiple)
-    locations_connect_check = Locations.objects.all()
     
 class SpecificationsForm(forms.ModelForm):
     class Meta:
